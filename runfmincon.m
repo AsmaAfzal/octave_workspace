a46cb8fd5e8e90cf3dd6a2818bfc088061be0bce
AsmaAfzal <asmaafzal5@gmail.com>
2015-06-23 01:16:33 +0100

    examples for fmincon

---
+++ runfmincon.m
@@ -0,0 +1,15 @@
+clc
+clear all
+%objective_function = @ (p) p(1)^2 + p(2)^2;
+% pin = [-2; 5];
+% constraint_function = @ (p) p(1)^2 + 1 - p(2);
+% [p, objf, cvg, outp] = nonlin_min (objective_function, pin, optimset ("equc", {constraint_function},"Algorithm","octave_sqp","ubound",[0;6]))
+
+f = @(x) -x(1) * x(2) * x(3);
+
+A = [-1 -2 -2; ...
+      1  2  2];
+b = [0;72];
+x0 = [10;10;10];    % Starting guess at the solution
+S=-A;
+[x,fval] = nonlin_min( f, x0, optimset ("inequc",{S',b},"Algorithm","octave_sqp") ) 
