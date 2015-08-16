3ef244d90734cb0c0420eeb81f3d3405a196f37e
AsmaAfzal <asmaafzal5@gmail.com>
2015-08-16 11:41:21 +0100

    Applied patch. Removed trailing spaces

--- statget.m
+++ statget.m
@@ -1,18 +1,18 @@
 ## Copyright (C) 2008-2015 Jaroslav Hajek
 ## Copyright (C) 2009 VZLU Prague
 ##
-## Octave is free software; you can redistribute it and/or modify it
+## This function is free software; you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 3 of the License, or (at
 ## your option) any later version.
 ##
-## Octave is distributed in the hope that it will be useful, but
+## This function is distributed in the hope that it will be useful, but
 ## WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ## General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not, see
+## along with this function; see the file COPYING.  If not, see
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
@@ -24,8 +24,10 @@
 ## If @var{parname} is not defined then return @var{default} if supplied,
 ## otherwise return an empty matrix.
 ##
-## Copied from Octave (was 'optimget') (Asma Afzal <asmaafzal5@gmail.com>).
+## @seealso{statset}
 ## @end deftypefn
+
+## Copied from Octave (was 'optimset') (Asma Afzal <asmaafzal5@gmail.com>).
 
 function retval = statget (options, parname, default)
 
@@ -41,7 +43,7 @@ function retval = statget (options, parname, default)
 
   if (nmatch == 1)
     parname = opts{idx};
-  elseif
+  elseif (nmatch == 0)
     warning ("statget: unrecognized option: %s", parname);
   else
     fmt = sprintf ("statget: ambiguous option: %%s (%s%%s)",
@@ -58,10 +60,8 @@ function retval = statget (options, parname, default)
 
 endfunction
 
-
 %!shared opts
 %! opts = statset ("tolx", 0.1, "maxit", 100);
-%!assert (statget (opts, "TolX"), 0.1)
 %!assert (statget (opts, "maxit"), 100)
 %!assert (statget (opts, "MaxITer"), 100)
 %!assert (statget (opts, "TolFun"), [])
@@ -73,5 +73,3 @@ endfunction
 %!error statget (1,2,3,4,5)
 %!error statget (1, "name")
 %!error statget (struct (), 2)
-%!warning <statget: ambiguous option: Max> (statget (opts, "Max"));
-
